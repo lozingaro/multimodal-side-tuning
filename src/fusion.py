03a6c219666c96df95e5c7fff25513319daeba32
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2020-04-14 22:19:11 +0200

    up

--- src/fusion.py
+++ src/fusion.py
@@ -31,6 +31,7 @@ num_classes = 10
 num_epochs = 100
 result_file = '/home/stefanopio.zingaro/Developer/multimodal-side-tuning/test/results_tobacco.csv'
 cm_file = '/home/stefanopio.zingaro/Developer/multimodal-side-tuning/test/confusion_matrices/fusion_256_tobacco.png'
+side_fc = 256
 
 # d_train = RvlDataset(f'{conf.rlv_img_root_dir}/train')
 # dl_train = DataLoader(d_train, batch_size=48, shuffle=True)
@@ -52,7 +53,7 @@ dl_test = DataLoader(d_test, batch_size=32, shuffle=False)
 train_targets = d_train.dataset.targets
 labels = d.classes
 
-model = FusionSideNetFc(300, num_classes=num_classes, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=256).to(conf.core.device)
+model = FusionSideNetFc(300, num_classes=num_classes, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=side_fc).to(conf.core.device)
 print(sum(p.numel() for p in model.parameters() if p.requires_grad))
 _, c = np.unique(np.array(d.targets)[d_train.indices], return_counts=True)
 weight = torch.from_numpy(np.min(c) / c).type(torch.FloatTensor).to(conf.device)
@@ -62,7 +63,7 @@ scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lambda epoch: .1 * (1.0
 pipeline = TrainingPipeline(model, criterion, optimizer, scheduler, device=conf.device, num_classes=num_classes)
 best_valid_acc, test_acc, cm, dist = pipeline.run(dl_train, dl_val, dl_test, num_epochs=num_epochs, classes=labels)
 
-s = f'1280x512x10,sgd,fasttext,min,3-3-4,' \
+s = f'1280x{side_fc}x10,sgd,fasttext,min,3-3-4,' \
     f'{best_valid_acc:.3f},' \
     f'{test_acc:.3f},' \
     f'{",".join([f"{r[i] / np.sum(r):.3f}" for i, r in enumerate(cm)])}\n'
