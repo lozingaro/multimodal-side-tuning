6ba81d7077d846c60cd2dbe57db0a30e6973916a
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2020-06-30 10:49:27 +0200

    quick fix and update

--- src/demo_mmst_tobacco.py
+++ src/demo_mmst_tobacco.py
@@ -46,22 +46,13 @@ print("""
 filterwarnings("ignore")
 cudnn.deterministic = True
 cudnn.benchmark = False
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-
 torch.manual_seed(42)
 np.random.seed(42)
 random.seed(42)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 d = TobaccoDataset(config.tobacco_img_root_dir, config.tobacco_txt_root_dir)
-d_train, d_val, d_test = torch.utils.data.random_split(d, [800, 200, 2482])
-dl_train = DataLoader(d_train, batch_size=16, shuffle=True)
-dl_val = DataLoader(d_val, batch_size=4, shuffle=True)
-dl_test = DataLoader(d_test, batch_size=32, shuffle=False)
-train_targets = d_train.dataset.targets
-labels = d.classes
-num_classes = len(np.unique(train_targets))
-num_epochs = 100
-criterion = nn.CrossEntropyLoss().to(device)
+num_classes = len(d.classes)
 
 for alphas in config.alphas:
     for model in (
@@ -71,11 +62,29 @@ for alphas in config.alphas:
         FusionSideNetFcResNet(300, num_classes=num_classes, alphas=alphas, dropout_prob=.5, side_fc=1024),
         # FusionSideNetFcVGG(300, num_classes=num_classes, alphas=alphas, dropout_prob=.5, side_fc=512),
     ):
+        torch.manual_seed(42)
+        np.random.seed(42)
+        random.seed(42)
+
+        d_train, d_val, d_test = torch.utils.data.random_split(d, [800, 200, 2482])
+        dl_train = DataLoader(d_train, batch_size=16, shuffle=True)
+        dl_val = DataLoader(d_val, batch_size=4, shuffle=True)
+        dl_test = DataLoader(d_test, batch_size=32, shuffle=False)
+
+        train_targets = d_train.dataset.targets
+        labels = d.classes
+        num_epochs = 100
+
         model = model.to(device)
-        optimizer = torch.optim.SGD(model.parameters(), lr=.1, momentum=.9)
-        scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer,
-                                                      lambda epoch: .1 * (1.0 - float(epoch) / float(num_epochs)) ** .5)
+        learning_rate = .1
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=.9)
+        criterion = nn.CrossEntropyLoss().to(device)
+        scheduler = torch.optim.lr_scheduler.LambdaLR(
+            optimizer,
+            lambda epoch: learning_rate * (1.0 - float(epoch) / num_epochs) ** .5
+        )
         pipeline = TrainingPipeline(model, criterion, optimizer, scheduler, device=device, num_classes=num_classes)
+
         since = time.time()
         best_valid_acc, test_acc, cm, dist = pipeline.run(dl_train, dl_val, dl_test, num_epochs=num_epochs,
                                                           classes=labels)
@@ -94,14 +103,14 @@ for alphas in config.alphas:
                     f'{test_acc:.3f},'
                     f'{",".join([f"{r[i] / np.sum(r):.3f}" for i, r in enumerate(cm)])}\n')
 
-        cm_file = f'../test/confusion_matrices/{model.name}_tobacco_{"-".join([str(i) for i in alphas])}.png'
-        fig, ax = plt.subplots(figsize=(8, 6))
-        ax.imshow(cm, aspect='auto', cmap=plt.get_cmap('Reds'))
-        plt.ylabel('Actual Category')
-        plt.yticks(range(len(cm)), labels, rotation=45)
-        plt.xlabel('Predicted Category')
-        plt.xticks(range(len(cm)), labels, rotation=45)
-        [ax.text(j, i, round(cm[i][j] / np.sum(cm[i]), 2), ha="center", va="center") for i in range(len(cm)) for j in
-         range(len(cm[i]))]
-        fig.tight_layout()
-        plt.savefig(cm_file)
+        # cm_file = f'../test/confusion_matrices/{model.name}_tobacco_{"-".join([str(i) for i in alphas])}.png'
+        # fig, ax = plt.subplots(figsize=(8, 6))
+        # ax.imshow(cm, aspect='auto', cmap=plt.get_cmap('Reds'))
+        # plt.ylabel('Actual Category')
+        # plt.yticks(range(len(cm)), labels, rotation=45)
+        # plt.xlabel('Predicted Category')
+        # plt.xticks(range(len(cm)), labels, rotation=45)
+        # [ax.text(j, i, round(cm[i][j] / np.sum(cm[i]), 2), ha="center", va="center") for i in range(len(cm)) for j in
+        #  range(len(cm[i]))]
+        # fig.tight_layout()
+        # plt.savefig(cm_file)
