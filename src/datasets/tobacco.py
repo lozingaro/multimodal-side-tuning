714febf0fe46cbd53fa8c5b3d4a1a88486479cc5
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2020-07-05 12:54:24 +0200

    plit tobacco dataset for train val and test procedure added

--- src/datasets/tobacco.py
+++ src/datasets/tobacco.py
@@ -46,75 +46,74 @@ class TobaccoDataset(torch.utils.data.Dataset):
         self.targets = []
         self.imgs = []
         self.txts = []
-        for i, label in enumerate(sorted(os.listdir(img_root_dir))):
-            txt_class_path = f'{txt_root_dir}/{label}'
-            img_class_path = f'{img_root_dir}/{label}'
-            self.classes += [label]
-            for img_path in os.scandir(img_class_path):
-                txt_path = f'{txt_class_path}/{".".join(img_path.name.split(".")[:-1])}.ptr'
-                self.targets += [i]
-                self.imgs += [img_path.path]
-                self.txts += [txt_path]
+        label_to_target = {}
+        i = 0
+        for label in sorted(os.listdir(img_root_dir)):
+            if os.path.isdir(f'{img_root_dir}/{label}'):
+                txt_class_path = f'{txt_root_dir}/{label}'
+                img_class_path = f'{img_root_dir}/{label}'
+                self.classes += [label]
+                label_to_target[label] = i
+                i += 1
+                for img_path in os.scandir(img_class_path):
+                    filename, file_extension = os.path.splitext(img_path.name)
+                    if file_extension.lower() in ['.tif', '.jpg']:
+                        txt_path = f'{txt_class_path}/{filename}.txt'
+                        if not os.path.isfile(txt_path) or not os.path.isfile(img_path.path):
+                            raise FileNotFoundError(f'Did not find {txt_path} or {img_path.path}!')
+                        self.targets += [label_to_target[label]]
+                        self.imgs += [img_path.path]
+                        self.txts += [txt_path]
 
     def __getitem__(self, item):
         img = TF.to_tensor(Image.open(self.imgs[item]))
-        txt = torch.load(self.txts[item]).float()
-        return (img, txt), self.targets[item]
+        # txt = torch.load(self.txts[item]).float()
+        return (img, self.txts[item]), self.targets[item]
 
     def __len__(self):
         return len(self.targets)
 
 
-class TobaccoImgDataset(torch.utils.data.Dataset):
-    def __init__(self, img_root_dir):
-        super(TobaccoImgDataset, self).__init__()
-        self.classes = []
-        self.targets = []
-        self.imgs = []
-        for i, label in enumerate(sorted(os.listdir(img_root_dir))):
-            img_class_path = f'{img_root_dir}/{label}'
-            self.classes += [label]
-            for img_path in os.scandir(img_class_path):
-                self.targets += [i]
-                self.imgs += [img_path.path]
-
-    def __getitem__(self, item):
-        img = TF.to_tensor(Image.open(self.imgs[item]))
-        return img, self.targets[item]
-
-    def __len__(self):
-        return len(self.targets)
-
-
-class TobaccoTxtDataset(torch.utils.data.Dataset):
-    def __init__(self, txt_root_dir):
-        super(TobaccoTxtDataset, self).__init__()
-        self.classes = []
-        self.targets = []
-        self.txts = []
-        for i, label in enumerate(sorted(os.listdir(txt_root_dir))):
-            txt_class_path = f'{txt_root_dir}/{label}'
-            self.classes += [label]
-            for txt_path in os.scandir(txt_class_path):
-                self.targets += [i]
-                self.txts += [txt_path.path]
-
-    def __getitem__(self, item):
-        txt = torch.load(self.txts[item]).float()
-        return txt, self.targets[item]
-
-
-if __name__ == '__main__':
+def split_tobacco(split_dir):
     def move(source, dest):
         from subprocess import call
-        call(["mv", source, dest])
+        call(["cp", source, dest])
         return None
-    d = TobaccoDataset(img_root_dir='../../data/Tobacco3482-jpg', txt_root_dir='../../data/QS-OCR-small')
+    d = TobaccoDataset(img_root_dir='/Volumes/SD128/Developer/ocrized-text-dataset/datasets/Tobacco3482', txt_root_dir='/Volumes/SD128/Developer/ocrized-text-dataset/datasets/QS-OCR-small')
     d_train, d_val, d_test = torch.utils.data.random_split(d, [800, 200, 2482])
+    if not os.path.isdir(split_dir):
+        os.mkdir(split_dir)
+        os.mkdir(f'{split_dir}/train')
+        os.mkdir(f'{split_dir}/train/img')
+        os.mkdir(f'{split_dir}/train/txt')
+        os.mkdir(f'{split_dir}/val')
+        os.mkdir(f'{split_dir}/val/img')
+        os.mkdir(f'{split_dir}/val/txt')
+        os.mkdir(f'{split_dir}/test')
+        os.mkdir(f'{split_dir}/test/img')
+        os.mkdir(f'{split_dir}/test/txt')
     for i in d_train.indices:
         l = d.classes[d[i][1]]
-        os.mkdir(f'../../data/tobacco/train/img/{l}')
-        os.mkdir(f'../../data/tobacco/train/txt/{l}')
-        move(d.imgs[i], f'../../data/tobacco/train/img/{l}/.')
-        move(d.txts[i], f'../../data/tobacco/train/txt/{l}/.')
+        if not os.path.isdir(f'{split_dir}/train/img/{l}'):
+            os.mkdir(f'{split_dir}/train/img/{l}')
+        if not os.path.isdir(f'{split_dir}/train/txt/{l}'):
+            os.mkdir(f'{split_dir}/train/txt/{l}')
+        move(d.imgs[i], f'{split_dir}/train/img/{l}/.')
+        move(d.txts[i], f'{split_dir}/train/txt/{l}/.')
+    for i in d_val.indices:
+        l = d.classes[d[i][1]]
+        if not os.path.isdir(f'{split_dir}/val/img/{l}'):
+            os.mkdir(f'{split_dir}/val/img/{l}')
+        if not os.path.isdir(f'{split_dir}/val/txt/{l}'):
+            os.mkdir(f'{split_dir}/val/txt/{l}')
+        move(d.imgs[i], f'{split_dir}/val/img/{l}/.')
+        move(d.txts[i], f'{split_dir}/val/txt/{l}/.')
+    for i in d_test.indices:
+        l = d.classes[d[i][1]]
+        if not os.path.isdir(f'{split_dir}/test/img/{l}'):
+            os.mkdir(f'{split_dir}/test/img/{l}')
+        if not os.path.isdir(f'{split_dir}/test/txt/{l}'):
+            os.mkdir(f'{split_dir}/test/txt/{l}')
+        move(d.imgs[i], f'{split_dir}/test/img/{l}/.')
+        move(d.txts[i], f'{split_dir}/test/txt/{l}/.')
 
