23e102c15f399eb7e7dcc5a065f57cef19894508
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2020-07-06 15:01:31 +0200

    rvl second round of test

--- src/conf/core.py
+++ src/conf/core.py
@@ -8,17 +8,14 @@ rlv_img_root_dir = '/data01/stefanopio.zingaro/datasets/RVL-CDIP'
 rlv_txt_root_dir = '/data01/stefanopio.zingaro/datasets/QS-OCR-Large'
 text_fasttext_model_path = '/data01/stefanopio.zingaro/datasets/word2vec/fasttext/cc.en.300.bin'
 
-rvl_labels = ['Letter', 'Form', 'Email', 'Handwritten', 'Advertisement', 'Scientific report', 'Scientific publication', 'Specification', 'File folder', 'News article', 'Budget', 'Invoice', 'Presentation', 'Questionnaire', 'Resume', 'Memo']
-tobacco_labels = ['Advertisement', 'Email', 'Form', 'Letter', 'Memo', 'News', 'Note', 'Report', 'Resume', 'Scientific']
-
 tasks_classifier = [
     # '1280x128x10',
     # '1280x256x10',
     # '1280x512x10',
-    'resnet',
+    # 'resnet',
     'mobilenet',
-    'resnet-direct',
-    'mobilenet-direct'
+    # 'resnet-direct',
+    # 'mobilenet-direct'
 ]
 tasks_optimizer = ['sgd']  # ['sgd', 'adam']
 tasks_embedding = ['fasttext']  # ['fasttext', 'custom']
@@ -28,16 +25,18 @@ tasks_loss_weigth = [
     # 'max'
 ]
 
-tasks_coeffs = ['2-3-5',
-                '2-4-4',
-                '2-5-3',
-                '3-2-5',
-                '3-3-4',
-                '3-4-3',
-                '3-5-2',
-                '4-2-4',
-                '4-3-3',
-                '4-4-2',
-                '5-2-3',
-                '5-3-2']
+tasks_coeffs = [
+    '2-3-5',
+    '2-4-4',
+    '2-5-3',
+    '3-2-5',
+    '3-3-4',
+    '3-4-3',
+    '3-5-2',
+    '4-2-4',
+    '4-3-3',
+    '4-4-2',
+    '5-2-3',
+    '5-3-2'
+]
 tasks = itertools.product(tasks_classifier, tasks_optimizer, tasks_embedding, tasks_loss_weigth, tasks_coeffs)
