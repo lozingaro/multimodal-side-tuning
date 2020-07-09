d4c0b35666007379f91d980297544f322682a5c3
Stefano Pio Zingaro <stefanopio.zingaro@unibo.it>
2020-07-09 08:47:47 +0200

    num params

--- src/test.py
+++ src/test.py
@@ -1,5 +1,14 @@
-from models import FusionSideNetFc
+from models import FusionSideNetFc, FusionSideNetFcResNet, MobileNet, ResNet
 
-model = FusionSideNetFc(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=1024)
-model = FusionSideResNet(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=1024)
+# model = FusionSideNetFc(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=512)
+# print(sum(p.numel() for p in model.parameters()))
+# model = FusionSideNetFc(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=1024)
+# print(sum(p.numel() for p in model.parameters()))
+# model = FusionSideNetFcResNet(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=512)
+# print(sum(p.numel() for p in model.parameters()))
+# model = FusionSideNetFcResNet(300, num_classes=10, alphas=[.3, .3, .4], dropout_prob=.5, side_fc=1024)
+# print(sum(p.numel() for p in model.parameters()))
+# model = MobileNet(10)
+# print(sum(p.numel() for p in model.parameters()))
+model = ResNet(10)
 print(sum(p.numel() for p in model.parameters()))
